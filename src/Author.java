4311659330f69dce258701d24864a4738cddfd3a
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-07-13 13:48:54 -0600

    annotations suck

--- src/Author.java
+++ src/Author.java
@@ -1,20 +1,21 @@
 /**
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
+
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-
         books = new ArrayList();
     }
 
     /**
-     * @deprecated Use publishedBooks instead
+     //* @deprecated Use publishedBooks instead
      */
     @Deprecated
     public List<String> getBooks() {
